d4e86633f677f47083859526a154ffbc2cdeb104
Salarific <salar.farsi@gmail.com>
2024-11-04 16:13:42 +0100

    inital Frontend Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Punchout Reports
+
+## Getting Started
+
+To start the development server, run:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
--- src/app/ui/main/visitorsCount/VisitorsCount.jsx
+++ src/app/ui/main/visitorsCount/VisitorsCount.jsx
@@ -4,40 +4,45 @@ import { IoPeopleSharp } from 'react-icons/io5';
 import styles from './visitorsCount.module.css';
 
 const VisitorCount = () => {
-  const backendURL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL || '';
+  const backendURL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
   const [visitorCount, setVisitorCount] = useState(null); // Initialize as null
+  const [error, setError] = useState(null); // To handle errors
 
   useEffect(() => {
-    // Retrieve visitor count from localStorage on client-side only
-    if (typeof window !== 'undefined') {
-      const savedCount = localStorage.getItem('visitorCount');
-      if (savedCount) setVisitorCount(parseInt(savedCount, 10));
-    }
-
     // Fetch the latest visitor count from the server
     const fetchVisitorCount = async () => {
+      if (!backendURL) {
+        console.error("Backend URL is not defined");
+        return;
+      }
+
       try {
         const response = await fetch(`${backendURL}/api/visitor-count`);
-        const data = await response.json();
-        setVisitorCount(data.count);
-
-        // Save latest count to localStorage
-        if (typeof window !== 'undefined') {
-          localStorage.setItem('visitorCount', data.count);
+        if (response.ok) {
+          const data = await response.json();
+          setVisitorCount(data.count);
+        } else {
+          console.error('Failed to fetch visitor count');
+          setError('Failed to load visitor count. Please try again later.');
         }
       } catch (error) {
-        console.error('Failed to fetch visitor count:', error);
+        console.error('Error fetching visitor count:', error);
+        setError('An error occurred while fetching visitor count.');
       }
     };
 
     fetchVisitorCount();
-  }, []);
+  }, [backendURL]);
 
   return (
     <div className={styles.container} title='Website Visitors'>
       <IoPeopleSharp size={20} className={styles.icon} />
       <div className={styles.counts}>
-        {visitorCount !== null ? visitorCount : <div></div>}
+        {error ? (
+          <span>{error}</span>
+        ) : (
+          visitorCount !== null ? visitorCount : <span></span>
+        )}
       </div>
     </div>
   );
